f763186230141202b242841e6dbd4b67cbae231b
somov <pavsome@mail.ru>
2021-02-26 19:03:53 +0300

    fixed bugs added jar config

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,21 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
 application {
     mainClassName = "MainKt"
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,77 +14,85 @@ const val helpString = "Choose mode:\n" +
 const val authorString = "©Vitaly Artemiev 2021"
 
 fun main(args: Array<String>) {
-    require(args.size >= 0) {"No arguments provided"}
+    if (args.size == 0) {
+        println("No arguments provided\n" +
+                helpString)
+    } else {
 
-    when (args[0]) {
-        "-reverse" -> {
-            for (s in args.drop(1).reversed()) {
-                println(s)
+        when (args[0]) {
+            "-reverse" -> {
+                for (s in args.drop(1).reversed()) {
+                    println(s)
+                }
             }
-        }
-        "-sum" -> {
-            println("Sum of args: " +
-                    "${args.drop(1).mapNotNull { it.toIntOrNull() }.sum()}")
-        }
-        "-mul" -> {
-            println("Product of args: " +
-                    "${args.drop(1).mapNotNull { it.toIntOrNull() }.reduce {acc, i -> acc * i}} ")
-        }
-        "-maxmin" -> {
-            val list = args.drop(1).mapNotNull { it.toIntOrNull() }
-            val max = list.maxOrNull()
-            val min = list.minOrNull()
-
-            if (max == null) {
-                println("No numbers found")
-            } else {
-                println("Max element: $max")
-                println("Min element: $min")
+            "-sum" -> {
+                println("Sum of args: " +
+                        "${args.drop(1).mapNotNull { it.toIntOrNull() }.sum()}"
+                )
             }
-        }
-        "-primes" -> {
-            val list = args.drop(1).mapNotNull { it.toIntOrNull() }.filter {
-                if (it > 0 ) {
-                    var flag = false
+            "-mul" -> {
+                println("Product of args: " +
+                        "${args.drop(1).mapNotNull { it.toIntOrNull() }.reduce { acc, i -> acc * i }} "
+                )
+            }
+            "-maxmin" -> {
+                val list = args.drop(1).mapNotNull { it.toIntOrNull() }
+                val max = list.maxOrNull()
+                val min = list.minOrNull()
 
-                    for (i in 2..it / 2) {
-                        if (it % i == 0) {
-                            flag = true
-                            break
+                if (max == null) {
+                    println("No numbers found")
+                } else {
+                    println("Max element: $max")
+                    println("Min element: $min")
+                }
+            }
+            "-primes" -> {
+                val list = args.drop(1).mapNotNull { it.toIntOrNull() }.filter {
+                    if (it > 0) {
+                        var flag = false
+
+                        for (i in 2..it / 2) {
+                            if (it % i == 0) {
+                                flag = true
+                                break
+                            }
                         }
+
+                        !flag
+                    } else {
+                        false
                     }
 
-                    !flag
-                } else {
-                    false
                 }
-
+                println("Prime numbers: ${list.toString()}")
             }
-            println("Prime numbers: ${list.toString()}")
-        }
-        "-halfsum" -> {
-            println("Elements that are a half-sum of their neighbors:")
+            "-halfsum" -> {
+                println("Elements that are a half-sum of their neighbors:")
 
-            val list = args.drop(1).mapNotNull { it.toIntOrNull() }.windowed(3,1)
-                .filter {
-                    it[1] == (it[0] + it[2])/2
+                val list = args.drop(1).mapNotNull { it.toIntOrNull() }.windowed(3, 1)
+                    .filter {
+                        it[1] == (it[0] + it[2]) / 2
+                    }
+
+                for (l in list) {
+                    println("${l[1]} = (${l[0]} + ${l[2]})/2")
                 }
-
-            for (l in list) {
-                println("${l[1]} = (${l[0]} + ${l[2]})/2")
             }
-        }
-        "-help" -> {
-            println(helpString)
-        }
-        "-author" -> {
-            val dt = LocalDateTime.now()
-            println(authorString + "\n" +
-                    "Current date: ${dt.toLocalDate().format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))}\n" +
-                    "Current time: ${dt.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"))}")
-        }
-        else -> {
-            println("Invalid mode.\n" + helpString)
+            "-help" -> {
+                println(helpString)
+            }
+            "-author" -> {
+                val dt = LocalDateTime.now()
+                println(
+                    authorString + "\n" +
+                            "Current date: ${dt.toLocalDate().format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))}\n" +
+                            "Current time: ${dt.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"))}"
+                )
+            }
+            else -> {
+                println("Invalid mode.\n" + helpString)
+            }
         }
     }
 }
