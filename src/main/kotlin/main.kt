97204f55e7092af83b20c1ac0b852855e6f907ce
somov <pavsome@mail.ru>
2021-02-26 18:41:06 +0300

    added -maxmin, -primes, -halfsum

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,6 +6,9 @@ const val helpString = "Choose mode:\n" +
         "-sum adds the rest of argument and outputs the result\n" +
         "-mul multiplies the rest of argument and outputs the result\n" +
         "-author displays author info and date/time\n" +
+        "-maxmin displays largest and smallest elements\n" +
+        "-primes displays only prime arguments\n" +
+        "-halfsum displays elements that are a half-sum of their neighbors\n" +
         "-help displays this message"
 
 const val authorString = "©Vitaly Artemiev 2021"
@@ -26,6 +29,50 @@ fun main(args: Array<String>) {
         "-mul" -> {
             println("Product of args: " +
                     "${args.drop(1).mapNotNull { it.toIntOrNull() }.reduce {acc, i -> acc * i}} ")
+        }
+        "-maxmin" -> {
+            val list = args.drop(1).mapNotNull { it.toIntOrNull() }
+            val max = list.maxOrNull()
+            val min = list.minOrNull()
+
+            if (max == null) {
+                println("No numbers found")
+            } else {
+                println("Max element: $max")
+                println("Min element: $min")
+            }
+        }
+        "-primes" -> {
+            val list = args.drop(1).mapNotNull { it.toIntOrNull() }.filter {
+                if (it > 0 ) {
+                    var flag = false
+
+                    for (i in 2..it / 2) {
+                        if (it % i == 0) {
+                            flag = true
+                            break
+                        }
+                    }
+
+                    !flag
+                } else {
+                    false
+                }
+
+            }
+            println("Prime numbers: ${list.toString()}")
+        }
+        "-halfsum" -> {
+            println("Elements that are a half-sum of their neighbors:")
+
+            val list = args.drop(1).mapNotNull { it.toIntOrNull() }.windowed(3,1)
+                .filter {
+                    it[1] == (it[0] + it[2])/2
+                }
+
+            for (l in list) {
+                println("${l[1]} = (${l[0]} + ${l[2]})/2")
+            }
         }
         "-help" -> {
             println(helpString)
